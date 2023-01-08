a2645ef3adc222c92c022a5f449c6a1e50624863
Jade Ohlhauser <jadeonly@gmail.com>
2023-01-08 11:23:12 -0700

    Aut@0 is working on dev

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,13 +1,36 @@
+import json
 from datetime import datetime
+from os import environ as env
+from urllib.parse import quote_plus, urlencode
 
-from flask import Flask, render_template
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, redirect, render_template, session, url_for
 
 from . import app
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+app.secret_key = env.get("APP_SECRET_KEY")
+
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
 
 
 @app.route("/secure/")
@@ -17,12 +40,32 @@ def secure():
 
 @app.route("/login/")
 def login():
-    return render_template("login.html")
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
 
 
-@app.route("/logout/")
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+
+
+@app.route("/logout")
 def logout():
-    return render_template("logout.html")
+    session.clear()
+    return redirect(
+        "https://" + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("home", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
 
 
 @app.route("/hello/")
